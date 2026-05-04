db64333b335fce217f0ebc88affe94588c95b2aa
Mikhail Angelov <mikhail.angelov@gmail.com>
2026-05-04 17:04:32 +0200

    Add .gitignore, remove compiled artifacts

--- dist/auth.d.ts
+++
@@ -1,5 +0,0 @@
-import { User } from './seed';
-export declare function findUserByEmail(email: string): User | undefined;
-export declare function findUserById(id: string): User | undefined;
-export declare function createUser(name: string, email: string, password: string, role?: string): User;
-export declare function validatePassword(password: string, hash: string): boolean;
--- dist/sessions.d.ts
+++
@@ -1,3 +0,0 @@
-import { Session } from './seed';
-export declare function getUserSessions(userId: string): Session[];
-export declare function saveSession(session: Session): void;
